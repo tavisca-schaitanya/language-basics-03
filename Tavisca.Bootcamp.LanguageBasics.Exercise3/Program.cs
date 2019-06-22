8a68c4069fdea46b20aa3888eeebe8352eaa5fc7
tavisca-schaitanya <schaitanya@tavisca.in>
2019-06-22 15:28:36 +0530

    After 1st attempt successful

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -38,10 +39,70 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
+        public static List<int> getMealsByMinNutrients(List<int> mealIndices, int[] nutrient)
+        {
+            //Find min nutrient among the indices in mealIndices
+            int minNutrient = nutrient.Where((value, index) => mealIndices.BinarySearch(index) >= 0).Min();
+            //Find the indices of min nutrient in nutrient array
+            return mealIndices.Where(index => nutrient[index] == minNutrient).ToList();
+        }
+
+        public static List<int> getMealsByMaxNutrients(List<int> mealIndices, int[] nutrient)
+        {
+            //Find max nutrient among the indices in mealIndices
+            int maxNutrient = nutrient.Where((value, index) => mealIndices.BinarySearch(index) >= 0).Max();
+            //Find the indices of max nutrient in nutrient array
+            return mealIndices.Where(index => nutrient[index] == maxNutrient).ToList();
+        }
+
+        public static int[] getCalories(int[] protein, int[] carbs, int[] fat)
+        {
+            int[] calories = new int[protein.Length];
+            int i;
+            for(i = 0; i < protein.Length; i++)
+                calories[i] = fat[i]*9 + (carbs[i] + protein[i])*5;
+            return calories;
+        }
+
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int [] dietMeals = new int[dietPlans.Length];
+
+            //Dictionary to access nutrients based on first character(To avoid if conditions)
+            Dictionary<char, int[]> nutrientsDict = new Dictionary<char, int[]>();
+            nutrientsDict.Add('P', protein);
+            nutrientsDict.Add('C', carbs);
+            nutrientsDict.Add('F', fat);
+            nutrientsDict.Add('T', getCalories(protein, carbs, fat));
+
+            int i;
+            for(i = 0; i < dietPlans.Length; i++)
+            {
+                //If there is no diet plan, then return 0
+                if(dietPlans[i] == "")
+                {
+                    dietMeals[i] = 0;
+                    continue;
+                }
+
+                //List to store indices of mealPlans(Initialy all meals are included)
+                List<int> mealIndices = Enumerable.Range(0, protein.Length).ToList();
+
+                foreach(char dietItem in dietPlans[i])
+                {
+                    if(Char.IsUpper(dietItem))
+                        mealIndices = getMealsByMaxNutrients(mealIndices, nutrientsDict[dietItem]);
+                    else
+                        mealIndices = getMealsByMinNutrients(mealIndices, nutrientsDict[Char.ToUpper(dietItem)]);
+
+                    //If there is no tie, stop furthur process
+                    if(mealIndices.Count == 1)
+                        break;
+                }
+                //Diet Meal should be the lowest indexed meal
+                dietMeals[i] = mealIndices[0];
+            }
+            return dietMeals;
         }
     }
 }
